fd5a1c375c0eaaf43ffec993dfb74bec43256c6d
Okoro John <okorojohndarlington@gmail.com>
2022-07-31 19:34:22 +0100

    Express/GraphQL backend project and client CRUD operations completed

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./shema/schema");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+const express = require("express");
+const app = express();
+
+connectDB();
+
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema,
+		graphiql: process.env.NODE_ENV === "development",
+	})
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} MODE on PORT ${PORT}`.yellow.bold
+	)
+);
